73e4ef97eee6c2fa0e67955f7dde52205f89e9a8
Mikhaïl <94171094+mikhailBenali@users.noreply.github.com>
2022-03-31 13:50:59 +0200

    Affichage du fond d'écran et du perso (en cours d'avancement)

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
+from glob import glob
 
 # Initialisation
+from pygame.time import delay
+
 pygame.init()
 
 # Initialisation de la fenêtre
 screen = pygame.display.set_mode((1920, 1080))  # Attention à bien rentrer un tuple
 
-# Initialisaiton de l'icône et du titre
+# Initialisaiton du titre
 pygame.display.set_caption("Pana pua")
-icone = pygame.image.load("fleche.jpg")
-pygame.display.set_icon(icone)
 
 tour_image = pygame.image.load("tour.png")
 tour_x = 50
@@ -17,6 +18,36 @@ largeur_tour = 201
 
 taille_tour = 256
 tour_y = 1080 - taille_tour
+
+background_image = pygame.image.load("background.jpg")
+background_x = 0
+background_x_change = 0
+
+
+# Personnage
+
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self):
+        self.images = [pygame.image.load(f) for f in glob(f"Persos/*.png")]
+        self.frame = 0
+
+    def afficher(self, x, y):
+        if self.frame < len(self.images):
+            screen.blit(self.images[self.frame], (x, y))
+            self.frame += 1
+            delay(50)
+        else:
+            self.frame = 0
+
+
+test = Sprite("New Piskel")
+
+perso_image = pygame.image.load("Perso.png")
+perso_x = 50
+perso_y = 840
+
+font = pygame.font.Font("freesansbold.ttf", 32)
 
 
 def tour(x, y):
@@ -28,9 +59,6 @@ running = True
 
 while running:
 
-    # Mise du background en violet (RGB)
-    screen.fill((125, 0, 175))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -38,6 +66,11 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-    tour(tour_x, tour_y)
+    screen.blit(background_image, (background_x, 0))
+    screen.blit(font.render(f"{pygame.mouse.get_pos()[0]},{pygame.mouse.get_pos()[1]}", True, (255, 0, 0)), (0, 0))  # Affiche la position de la souris
+
+    screen.blit(perso_image, (perso_x, perso_y))
+
+    test.afficher(200, 200)
 
     pygame.display.update()
